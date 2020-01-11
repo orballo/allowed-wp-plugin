d63d92a13c0180e0a56956df3a58efc02e3e43d3
Eduardo Campaña <asrielo@gmail.com>
2020-01-11 21:42:07 +0400

    wip

--- class-aloud-plugin.php
+++ class-aloud-plugin.php
@@ -2,24 +2,26 @@
 
 class Aloud_Plugin
 {
+
+  function activate()
+  {
+  }
+
+  function deactivate()
+  {
+  }
+
   static function install()
   {
     $instance = new static();
 
     register_activation_hook(__FILE__, array($instance, 'activate'));
     register_deactivation_hook(__FILE__, array($instance, 'deactivate'));
-    register_uninstall_hook(__FILE__, array($instance, 'uninstall'));
-  }
-
-  static function activate()
-  {
-  }
-
-  static function deactivate()
-  {
+    register_uninstall_hook(__FILE__, array('Aloud_Plugin', 'uninstall'));
   }
 
   static function uninstall()
   {
+    set_transient("aloud_uninstall", "toma ya", 30);
   }
 }
